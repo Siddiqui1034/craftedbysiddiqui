a2257b3d21f37de02e5f891d2f1b11bbcfc36fba
siddiqui1034 <nausheensiddiqui11@gmail.com>
2025-07-04 16:01:39 +0530

    full responsive about

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -20,59 +20,58 @@ const MainLayout: FC<ChildrenInterface> = ({ children }) => {
         <Navbar />
 
         {/* Main section */}
-        <main className="grid grid-cols-1 lg:grid-cols-[1fr_3fr_0.75fr] text-2xl mx-[20px]  lg:pt-[50px] md:px-[20px] lg:px-[150px] mb-[10px] ">
+        <main className="grid grid-cols-1 xl:grid-cols-[1fr_3fr_0.75fr] text-2xl mx-[20px] xl:pt-[50px] lg:px-[150px] mb-[10px] ">
           {/* left aside */}
           <aside
-            className="sticky lg:top-[30px] md:flex md:flex-row md:w-full md:h-[500px] md:mx-auto md:p-[30px] bg-white border-2 border-gray-300 px-[15px] py-[20px] -mt-20 md:-mt-65 space-y-10 md:space-x-4 text-start md:text-center rounded-lg"
+            className="sticky xl:top-[30px] flex flex-col md:flex-row lg:flex-col lg:items-center md:gap-4 md:items-stretch 
+            lg:justify-center  md:justify-around md:h-[450px] lg:h-[650px] xl:h-[650px] md:w-full  bg-white border-2 border-gray-300 px-[15px] py-[20px] 
+           -mt-20 lg:-mt-0 xl:-mt-50 space-y-10 md:space-y-6 lg:space-y-4 text-start lg:text-center rounded-lg"
           >
+            <div className="relative w-full md:w-1/2 xl:w-full lg:flex lg:flex-col lg:justify-center xl:flex-1 h-[434px] md:h-full" >
             {/* Profile Image */}
-              <div className="relative h-[434px] md:h-full w-full md:flex-1">
-              {/* Profile Image */}
-                <Image
-                  fill
-                  className="rounded-lg object-cover"
-                  alt="profile"
-                  src="/images/profile.png"
-                />
-              </div>
-
-            {/* other information */}
-              <div className="space-y-4 md:flex-1">
+              <Image
+                fill
+                className="rounded-lg object-cover"
+                alt="profile"
+                src="/images/profile.png"
+              />
+            </div>
+            <div className="space-y-4 lg:flex-1 xl:flex-1 md:flex md:flex-col md:justify-center md:gap-10 lg:gap-2">
             {/* Name & Role */}
-              <div className="space-y-2 px-5">
-                <p className="text-2xl font-bold font-rubik">Nausheen Siddiqui</p>
-                <p className="text-lg text-gray-800 font-rubik">
-                  Software Engineer
-                </p>
-              </div>
+            <div className="space-y-2">
+              <p className="text-2xl font-bold font-rubik">Nausheen Siddiqui</p>
+              <p className="text-lg text-gray-800 font-rubik">
+                Software Engineer
+              </p>
+            </div>
             {/* Contact Info */}
-              <div className="bg-zinc-100 rounded-lg md:px-1 py-4 px-5 font-rubik text-gray-800 tracking-wide text-base">
-                <p>
-                  Phone:{" "}
-                  <span className="hover:text-rose-400 hover:underline">
-                    +01234567890
-                  </span>
-                </p>
-                <p>Email: siddiqui.techsunset@gmail.com</p>
-              </div>
+            <div className="bg-zinc-100 rounded-lg md:px-1 py-4 px-5 font-rubik text-gray-800 tracking-wide text-base">
+              <p>
+                Phone:{" "}
+                <span className="hover:text-rose-400 hover:underline">
+                  +01234567890
+                </span>
+              </p>
+              <p>Email: siddiqui.techsunset@gmail.com</p>
+            </div>
             {/* Button */}
-              <div>
-                <CustomButton icon={<ArrowRightOutlined />} className="w-full">
-                  Download My CV
-                </CustomButton>
-              </div>
+            <div>
+              <CustomButton icon={<ArrowRightOutlined />} className="w-full">
+                Download My CV
+              </CustomButton>
+            </div>
             </div>
           </aside>
 
 
-          <div className="shadow-lg rounded-lg my-[30px] p-10">
+          <div className="shadow-lg rounded-lg my-[30px] xl:mx-[25px] p-10">
             {children}
           </div>
 
           <div className="">
             {/* right aside */}
-            <aside className="sticky md:top-[30px]">
-              <div className="border-2 border-gray-300 rounded-lg p-[10px] lg:p-[20px] flex flex-row lg:flex-col lg:gap-8 justify-start gap-4 lg:justify-between">
+            <aside className="sticky lg:top-[30px]">
+              <div className="border-2 border-gray-300 rounded-lg p-[20px] flex lg:flex-col gap-2 lg:justify-between">
                 <RightSidebarButton href="/about"> About </RightSidebarButton>
                 <RightSidebarButton href="/resume"> Resume</RightSidebarButton>
                 <RightSidebarButton href="/work"> Work </RightSidebarButton>
